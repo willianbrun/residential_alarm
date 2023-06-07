27a259792e3826f07594ee15f2fe4c90fc9f4676
Willian Brun <willian.brun@stara.com.br>
2023-06-07 20:17:39 -0300

    First commit

---
+++ residential_alarm/residential_alarm.ino
@@ -0,0 +1,14 @@
+int alarmePortaSala = 2;
+int alarmeJanelaSala = 3;
+int alarmePortaCozinha = 4;
+int alarmeJanelaCozinha = 5;
+
+void setup() {
+  pinMode(alarmePortaaSala, INPUT_PULLUP);
+  pinMode(alarmeJanelaSala, INPUT_PULLUP);
+  pinMode(alarmePortaCozinha, INPUT_PULLUP);
+  pinMode(alarmeJanelaCozinha, INPUT_PULLUP);
+}
+
+void loop() {
+}
